01a3d499114396a7c30a2861aa2d671c4ab7871e
Mohmed Shawky <57459035+mohamedshawky70@users.noreply.github.com>
2025-03-15 17:15:18 +0200

    Update FilesController.cs

--- FileManagment.API/Controllers/FilesController.cs
+++ FileManagment.API/Controllers/FilesController.cs
@@ -11,35 +11,35 @@ namespace FileManagment.API.Controllers
     {
         private readonly IFileService _fileService= fileService;
         [HttpPost("upload")]
-		public async Task<IActionResult> Upload([FromForm]UploadFileRequest request,CancellationToken cancellationToken)
+	public async Task<IActionResult> Upload([FromForm]UploadFileRequest request,CancellationToken cancellationToken)
         {
-            var fileId = await _fileService.UploadAsync(request.File, cancellationToken);
-			//To reach to file by URL===> https://localhost:7097/api/Files/download/01959792-f2c0-7e28-8f14-00dbac1afc3b this link is in file header (Location)
-			return CreatedAtAction(nameof(Download), new {id=fileId},null);
+             var fileId = await _fileService.UploadAsync(request.File, cancellationToken);
+		//To reach to file by URL===> https://localhost:7097/api/Files/download/01959792-f2c0-7e28-8f14-00dbac1afc3b this link is in file header (Location)
+	     return CreatedAtAction(nameof(Download), new {id=fileId},null);
         }
         [HttpPost("upload-many")]
-		public async Task<IActionResult> UploadMany([FromForm] UploadManyFilesRequest request,CancellationToken cancellationToken)
+	public async Task<IActionResult> UploadMany([FromForm] UploadManyFilesRequest request,CancellationToken cancellationToken)
         {
             var filesIds = await _fileService.UploadManyAsync(request.Files, cancellationToken);
             return Ok(filesIds);
         }
         [HttpPost("upload-image")]
-		public async Task<IActionResult> UploadImage([FromForm] UploadImageRequest request,CancellationToken cancellationToken)
+	public async Task<IActionResult> UploadImage([FromForm] UploadImageRequest request,CancellationToken cancellationToken)
         {
             await _fileService.UploadImageAsync(request.Image, cancellationToken);
             return Created();
         }
         [HttpGet("download/{id}")]
-		public async Task<IActionResult> Download([FromRoute]Guid id,CancellationToken cancellationToken)
+	public async Task<IActionResult> Download([FromRoute]Guid id,CancellationToken cancellationToken)
         {
             var(fileContent, ContentType, fileName) =await _fileService.Download(id, cancellationToken);
             return fileContent is [] ? NotFound() : File(fileContent, ContentType, fileName);
-		}
+	}
         [HttpGet("stream/{id}")]
-		public async Task<IActionResult> Stream([FromRoute]Guid id,CancellationToken cancellationToken)
+	public async Task<IActionResult> Stream([FromRoute]Guid id,CancellationToken cancellationToken)
         {
             var(fileStream, ContentType, fileName) =await _fileService.Stream(id, cancellationToken);
             return fileStream is null ? NotFound() : File(fileStream, ContentType, fileName,enableRangeProcessing:true);
-		}
+	}
     }
 }
